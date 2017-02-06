e36e5235fee177e8cdc656fc4eb6fa21f2f1158b
Alena Serhiyenia <alena_serhiyenia@epam.com>
2017-02-06 12:08:17 +0300

    Fix for broken styles on direct urls

--- app.js
+++ app.js
@@ -34,6 +34,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({ secret: 'SECRET' }));
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
+
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'angular'),
   dest: path.join(__dirname, 'angular'),
